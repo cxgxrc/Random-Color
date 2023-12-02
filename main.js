bc64cb4678adbb046ac1bea3b4550e46599311ae
Lau PC <lautaro.tupac.casagrande@gmail.com>
2023-12-01 22:43:46 -0300

    control

--- main.js
+++ main.js
@@ -12,17 +12,35 @@ function generateRadomness() {
     let number = getRandomIntInclusive(0, 15);
     if (number > 9) {
         let i = number - 10
-        console.log(arrayLetters[i])
         return arrayLetters[i]
     } else {
-        console.log(number)
         return number
     }
 }
 
 // test 
 
-for (let index = 0; index < 10; index++) {
-    generateRadomness()
+// for (let index = 0; index < 10; index++) {
+//     getHexaColor()
     
+// }
+
+function getHexaColor() {
+    let color = "#";
+    for (let index = 0; index < 6; index++) {
+        color = color + generateRadomness();
+        return color
+    }
+}
+
+const background = document.getElementById('color')
+
+const btn = document.getElementById('interface')
+btn.addEventListener("click", changeColor)
+
+function changeColor() {
+    let i = getHexaColor()
+    background.style.backgroundColor = i
+    console.log(background.style)
+
 }
