cfb87c55dbfd7c16d79cdd6815c881648e644123
Lau PC <lautaro.tupac.casagrande@gmail.com>
2023-12-05 12:40:20 -0300

    Finish

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 // Generate a random number or digital up to f
 
-const arrayLetters = ['a', 'b', 'c', 'd', 'e', 'f']
+const arrayLetters = ['A', 'B', 'C', 'D', 'E', 'F']
+const background = document.getElementById('color')
+const random = document.getElementById('change')
+const btnn = document.getElementById('changeColor')
+
+// generate random number
 
 function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1) + min);
   }
+
+
+// Get a random number between 0 to 9 or a letter btw A to F 
 
 function generateRadomness() {
     let number = getRandomIntInclusive(0, 15);
@@ -15,32 +23,30 @@ function generateRadomness() {
         return arrayLetters[i]
     } else {
         return number
+        
     }
 }
 
-// test 
 
-// for (let index = 0; index < 10; index++) {
-//     getHexaColor()
-    
-// }
+// get a six digit color
 
 function getHexaColor() {
     let color = "#";
     for (let index = 0; index < 6; index++) {
         color = color + generateRadomness();
-        return color
+        
     }
+    return color
 }
 
-const background = document.getElementById('color')
 
-const btn = document.getElementById('interface')
-btn.addEventListener("click", changeColor)
+// If u click the btn the background of the page will change
+
+btnn.addEventListener("click", changeColor)
 
 function changeColor() {
     let i = getHexaColor()
-    background.style.backgroundColor = i
-    console.log(background.style)
+    random.textContent = i;
+    background.style.backgroundColor = i;
 
 }
